efb34fb755da19da8d304726e61b184ad0a8f607
Collin Pham <collinpham2018@u.northwestern.edu>
2016-10-30 12:34:49 -0500

    Updated Index Controller

--- app/FoodChoice/scripts/IndexController.js
+++ app/FoodChoice/scripts/IndexController.js
@@ -2,5 +2,24 @@ angular
   .module('FoodChoice')
   .controller('IndexController', function($scope, supersonic) {
     supersonic.ui.tabs.hide();
-    // Controller functionality here
+    
+    $scope.FoodChoices = [
+      { 
+        "Cookie": 0
+      },
+      {
+        "Carrot": 1
+      }
+    ]
+
+    $scope.returnFoodScore = function (foodName) {
+      const foodNames = Object.keys($scope.FoodChoices)
+
+      for (i = 0; i < $scope.FoodChoices.length; i++) {
+        if (foodNames[i] === foodName) {
+          return $scope.FoodChoices[i][foodName]
+        }
+      }
+    }
+
   });
