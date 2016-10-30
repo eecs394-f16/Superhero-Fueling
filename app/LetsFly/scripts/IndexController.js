11d71f8a5849b38a9381aa4ee189dad4690bb855
Aagam Shah <aagam.shah1210@gmail.com>
2016-10-30 12:45:53 -0500

    Added the GIF page module

--- app/LetsFly/scripts/IndexController.js
+++ app/LetsFly/scripts/IndexController.js
@@ -2,7 +2,7 @@ angular
   .module('LetsFly')
   .controller('IndexController', function($scope, supersonic) {
     // Controller functionality here
-    $scope.sendChoice = function () {
-    	supersonic.logger.log("Fly clicked");
-    }
+    // $scope.sendChoice = function () {
+    // 	supersonic.logger.log("Fly clicked");
+    // }
   });
