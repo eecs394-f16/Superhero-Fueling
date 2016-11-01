7b4674da304d8fac05e4e95fe4ce2653c24eadfd
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-01 12:18:27 -0500

    Remove unnecessary Let's Fly module.

--- app/LetsFly/index.js
+++
@@ -1,4 +0,0 @@
-angular.module('LetsFly', [
-  // Declare any module-specific AngularJS dependencies here
-  'common'
-]);
--- app/LetsFly/scripts/IndexController.js
+++
@@ -1,8 +0,0 @@
-angular
-  .module('LetsFly')
-  .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
-    // $scope.sendChoice = function () {
-    // 	supersonic.logger.log("Fly clicked");
-    // }
-  });
