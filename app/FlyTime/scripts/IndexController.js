642400de3c4d7cc5260c96a46a435a7db648f9da
Aagam Shah <aagam.shah1210@gmail.com>
2016-11-02 02:32:17 -0500

    Implemented GIF selection based on choice of food

--- app/FlyTime/scripts/IndexController.js
+++ app/FlyTime/scripts/IndexController.js
@@ -2,4 +2,11 @@ angular
   .module('FlyTime')
   .controller('IndexController', function($scope, supersonic) {
     $scope.score = steroids.view.params.score;
+    
+    if ($scope.score == 0){
+    	document.getElementById("flygif").innerHTML = '<img src="http://localhost/images/unsuccessful.gif" alt="Image not found" style="width:340px;height:540px;">';
+    }
+    else if ($scope.score == 1){
+    	document.getElementById("flygif").innerHTML = '<img src="http://localhost/images/successful.gif" alt="Image not found" style="width:340px;height:540px;">';    	
+    }
   });
