444d0973097fbbe88dbf1742030f3a9a4419b6b2
agamgupta <agamgupta.96@gmail.com>
2016-11-04 17:50:27 -0500

    All page and playagain implemented

--- app/powerpage/scripts/IndexController.js
+++ app/powerpage/scripts/IndexController.js
@@ -1,5 +1,10 @@
 angular
   .module('powerpage')
-  .controller('IndexController', function($scope, supersonic) {
+  .controller('IndexController', function($scope, $timeout, supersonic) {
     $scope.carryScore = steroids.view.params.carryScore
+
+	$timeout(function() {
+    	var view = new supersonic.ui.View("playagain#index");
+		supersonic.ui.layers.push(view);
+	}, 2000);
   });
