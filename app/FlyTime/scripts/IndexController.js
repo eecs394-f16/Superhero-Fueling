243d8f3752c83e0c9b663ef2cc94fd824efac48e
agamgupta <agamgupta.96@gmail.com>
2016-11-04 17:28:47 -0500

    timeout implemented

--- app/FlyTime/scripts/IndexController.js
+++ app/FlyTime/scripts/IndexController.js
@@ -1,5 +1,10 @@
 angular
   .module('FlyTime')
-  .controller('IndexController', function($scope, supersonic) {
+  .controller('IndexController', function($scope, $timeout, supersonic) {
     $scope.score = steroids.view.params.score;
+    $timeout(function() {
+  //   	var view = new supersonic.ui.View("powerpage#index");
+		// supersonic.ui.layers.push(view);
+		$scope.up = "hi"
+    }, 4000);
   });
