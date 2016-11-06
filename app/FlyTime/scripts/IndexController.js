46f8c87a900944f4f82f5baae0aff51f0db7d1f9
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-06 01:51:34 -0600

    Hide nav bar in gif view.

--- app/FlyTime/scripts/IndexController.js
+++ app/FlyTime/scripts/IndexController.js
@@ -1,20 +1,21 @@
 angular
   .module('FlyTime')
   .controller('IndexController', function($scope, $timeout, supersonic) {
+		supersonic.ui.navigationBar.hide({animated: false});
+
     $scope.score = steroids.view.params.score;
-    if($scope.score==1){
+
+    if ($scope.score == 1) {
     	$timeout(function() {
 	    	var myscore = $scope.score
 	    	var view = new supersonic.ui.View("powerpage#index?carryScore=1");
-			supersonic.ui.layers.push(view);
+				supersonic.ui.layers.push(view);
     	}, 4000);
-    }
-    
-    else if($scope.score==0){
+    } else if ($scope.score == 0) {
     	$timeout(function() {
 	    	var myscore = $scope.score
 	    	var view = new supersonic.ui.View("powerpage#index?carryScore=0");
-			supersonic.ui.layers.push(view);
+				supersonic.ui.layers.push(view);
     	}, 6000);
     }
   });
