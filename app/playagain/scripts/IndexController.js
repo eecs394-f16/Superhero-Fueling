bab397bc129f48699c2a3a83cc527894ac74b685
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-06 02:05:10 -0600

    Refactor to pop all view layers instead of adding a new home view layer.

--- app/playagain/scripts/IndexController.js
+++ app/playagain/scripts/IndexController.js
@@ -2,4 +2,8 @@ angular
   .module('playagain')
   .controller('IndexController', function($scope, supersonic) {
     supersonic.ui.navigationBar.hide({animated: false});
+
+    $scope.backToHome = function() {
+      supersonic.ui.layers.popAll();
+    };
   });
