cf4054aa2a3f80751e34973256f0059ad3ac2140
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-06 01:53:49 -0600

    Remove nav bar from powerbar view.

--- app/powerpage/scripts/IndexController.js
+++ app/powerpage/scripts/IndexController.js
@@ -1,10 +1,12 @@
 angular
   .module('powerpage')
   .controller('IndexController', function($scope, $timeout, supersonic) {
+    supersonic.ui.navigationBar.hide({animated: false});
+
     $scope.carryScore = steroids.view.params.carryScore
 
-	$timeout(function() {
-    	var view = new supersonic.ui.View("playagain#index");
-		supersonic.ui.layers.push(view);
-	}, 2000);
+    $timeout(function() {
+      var view = new supersonic.ui.View("playagain#index");
+      supersonic.ui.layers.push(view);
+    }, 2000);
   });
