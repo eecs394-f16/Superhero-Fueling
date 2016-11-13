1a812b91e82ba4141da58252913313d243a08fc3
Aagam Shah <aagam.shah1210@gmail.com>
2016-11-13 12:29:43 -0600

    added kryptonite image module and food facts module

---
+++ app/foodfacts/index.js
@@ -0,0 +1,4 @@
+angular.module('foodfacts', [
+  // Declare any module-specific AngularJS dependencies here
+  'common'
+]);
--- app/powerpage/scripts/IndexController.js
+++ app/powerpage/scripts/IndexController.js
@@ -4,9 +4,13 @@ angular
     supersonic.ui.navigationBar.hide({animated: false});
 
     $scope.carryScore = steroids.view.params.carryScore
+    
+    $scope.backToHome = function() {
+      supersonic.ui.layers.popAll();
+    };
 
-    $timeout(function() {
-      var view = new supersonic.ui.View("playagain#index");
-      supersonic.ui.layers.push(view);
-    }, 2000);
+    // $timeout(function() {
+    //   var view = new supersonic.ui.View("playagain#index");
+    //   supersonic.ui.layers.push(view);
+    // }, 2000);
   });
