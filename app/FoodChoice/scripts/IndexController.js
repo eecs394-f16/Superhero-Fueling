4a48d903d308fddd16f2a37361f392d174069751
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-19 17:58:41 -0600

    Refactor to use array.find

--- app/FoodChoice/scripts/IndexController.js
+++ app/FoodChoice/scripts/IndexController.js
@@ -18,36 +18,36 @@ angular
     $scope.selected = [];
 
     $scope.toggleFood = function(id) {
-      $scope.foodChoices.forEach(function(food) {
-        if (food.id == id) {
-          if ($scope.isSelected(food.id)) {
-            $scope.selectedScore -= food.Value;
-            $scope.selected.splice($scope.selected.indexOf(food.id), 1);
-          } else {
-            if ($scope.selected.length == 3) {
-              var options = {
-                message: "You can only select up to 3 foods.",
-                buttonLabel: "Close"
-              };
-
-              supersonic.ui.dialog.alert("Too many foods!", options);
-            } else {
-              $scope.selectedScore += food.Value;
-              $scope.selected.push(food.id);
-            }
-          }
-          // Update score
-          $scope.selectedScoreAvg = Math.floor($scope.selectedScore / $scope.selected.length);
-          // Update navigation bar title
-          var title_str;
-          if ($scope.selected.length == 3) {
-            title_str = 'Are you ready to fly?';
-          } else {
-            title_str = 'Choose up to ' + parseInt(3 - $scope.selected.length) + ' food(s)!';
-          }
-          supersonic.ui.navigationBar.update({title: title_str});
-        }
+      var clickedFood = $scope.foodChoices.find(function(food) {
+        return food.id === id;
       });
+
+      if ($scope.isSelected(clickedFood.id)) {
+        $scope.selectedScore -= clickedFood.Value;
+        $scope.selected.splice($scope.selected.indexOf(clickedFood.id), 1);
+      } else {
+        if ($scope.selected.length == 3) {
+          var options = {
+            message: "You can only select up to 3 foods.",
+            buttonLabel: "Close"
+          };
+
+          supersonic.ui.dialog.alert("Too many foods!", options);
+        } else {
+          $scope.selectedScore += clickedFood.Value;
+          $scope.selected.push(clickedFood.id);
+        }
+      }
+      // Update score
+      $scope.selectedScoreAvg = Math.floor($scope.selectedScore / $scope.selected.length);
+      // Update navigation bar title
+      var title_str;
+      if ($scope.selected.length == 3) {
+        title_str = 'Are you ready to fly?';
+      } else {
+        title_str = 'Choose up to ' + parseInt(3 - $scope.selected.length) + ' food(s)!';
+      }
+      supersonic.ui.navigationBar.update({title: title_str});
     };
 
     $scope.isSelected = function(id) {
