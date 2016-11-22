4062e5399cd15523c988382f3b0dec2e2fd146d3
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-22 04:50:45 -0600

    Refactor to not use ng-repeat.

--- app/foodfacts/scripts/IndexController.js
+++ app/foodfacts/scripts/IndexController.js
@@ -1,19 +1,16 @@
 angular
   .module('foodfacts')
   .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
     supersonic.ui.navigationBar.hide({animated: false});
 
-    $scope.selected = [steroids.view.params.selectedId]
-    $scope.data = []
+    $scope.selected = steroids.view.params.selectedId;
+    $scope.data = {};
 
     var foodData = supersonic.data.model('FoodData');
-    $scope.selected.forEach(function(item) {
-      foodData.find(item).then(function(data) {
-        $scope.data.push(data);
+      foodData.find($scope.selected).then(function(data) {
+        $scope.data = data;
         supersonic.logger.log($scope.data); // TODO: Why does the absence of this line break the app?
       });
       supersonic.logger.log($scope.data);
-    });
 
   });
