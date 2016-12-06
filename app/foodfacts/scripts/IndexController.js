be8441ad203a8b2939db5f2dbc5b235f3c46eb16
Collin Pham <collinpham2018@u.northwestern.edu>
2016-12-05 19:53:28 -0600

    Added comments

--- app/foodfacts/scripts/IndexController.js
+++ app/foodfacts/scripts/IndexController.js
@@ -1,11 +1,14 @@
 angular
   .module('foodfacts')
   .controller('IndexController', function($scope, supersonic, $http) {
+    // Hide nav bar
     supersonic.ui.navigationBar.hide({animated: false});
 
+    // Store the passed id
     $scope.selected = steroids.view.params.selectedId;
     $scope.data = {};
 
+    // Get the data of the selected food from the stored id
     $http.get('/data/data.json').then(function(response) {
       response.data.forEach(function(food) {
         if (food.id == $scope.selected) {
